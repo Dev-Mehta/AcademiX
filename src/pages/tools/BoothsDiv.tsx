873f5c629d40804b5b50a9c300cb014db7ab318b
kszala <krushnadevzala5@gmail.com>
2024-08-22 13:34:47 +0530

    Fixed error

--- src/pages/tools/BoothsDiv.tsx
+++ src/pages/tools/BoothsDiv.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
 import { FormEvent, useEffect, useState } from 'react';
 
 const subtractBinary = (a: string, b: string) => {
