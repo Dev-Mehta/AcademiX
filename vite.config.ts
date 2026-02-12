c3c6d2ba44c97ef8aa7b9cb587f72e89fbbe37b4
Meetrajsinh Zala <zalameetraj07@gmail.com>
2026-02-12 19:48:47 +0530

    UI update and Resposivness

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ["1427-2402-a00-402-cd06-6d9f-f214-41e2-34a7.ngrok-free.app"],
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
-    }
+    },
   },
   plugins: [react()],
   resolve: {
@@ -16,6 +19,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',
-  assetsInclude: ['**/*.md', '**/*.mdx'],
-})
+  base: "/",
+  assetsInclude: ["**/*.md", "**/*.mdx"],
+});
